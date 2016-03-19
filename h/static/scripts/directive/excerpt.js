9d77b8f6342f22f68a9c2ef7b885d8629f96d2e1
Robert Knight <robertknight@gmail.com>
2016-03-19 15:46:18 +0000

    Re-implement excerpt overflow state recalculation
    Previously the overflowing state of the excerpt was computed implicitly
    by the overflowing() method, which was called from several other
    controller functions.
    
    There were several problems with this approach:
    
     1. Since this method calls Element.scrollHeight, it triggered an
        expensive synchronous layout flush.
     2. In certain situations, this could trigger a loop where the
        overflowing flag was continually flipped (see #2960)
     3  It was unpredictable when the overflowing state would be recomputed.
    
    This commit instead opts for a different approach which is more explicit
    about when the state is recomputed, based on the assumption that the
    <excerpt>'s content size is a function of:
    
     1. The inputs to the <excerpt>'s content (eg. the text of a quote, the body
        of an annotation)
     2. The sidebar window's size
     3. The dimensions of any embedded media
    
    The content size is then computed and updated as follows:
    
     1. When the <excerpt> is initially created, an async computation of
        the scrollHeight is scheduled. The calculation unfortunately needs
        to be async because the content may contain Angular directives which
        take several $digest cycles to fully process.
    
     2. The <excerpt> listens for changes to the content's data (via a
        'contentData' property), media 'load' events and window 'resize'
        events and recomputes the overflow state in response to any of
        these.
    
    Fixes #2960

--- h/static/scripts/directive/excerpt.js
+++ h/static/scripts/directive/excerpt.js
@@ -14,11 +14,11 @@ function ExcerptController() {
   };
 
   this.isExpandable = function () {
-    return this.overflowing() && this.collapse;
+    return this.overflowing && this.collapse;
   };
 
   this.isCollapsible = function () {
-    return this.overflowing() && !this.collapse;
+    return this.overflowing && !this.collapse;
   };
 
   this.toggle = function (event) {
@@ -37,7 +37,7 @@ function ExcerptController() {
   };
 
   this.showInlineControls = function () {
-    return this.overflowing() && this.inlineControls;
+    return this.overflowing && this.inlineControls;
   };
 
   this.bottomShadowStyles = function () {
@@ -69,22 +69,98 @@ function excerpt() {
     controllerAs: 'vm',
     link: function (scope, elem, attrs, ctrl) {
       var contentElem;
+      var pendingOverflowCheck = false;
+
+      /**
+       * Recompute whether the excerpt's content is overflowing the collapsed
+       * element.
+       *
+       * This check is scheduled manually in response to changes in the inputs
+       * to this component and certain events to avoid excessive layout flushes
+       * caused by accessing the element's size.
+       */
+      function recomputeOverflowState() {
+        if (!pendingOverflowCheck) {
+          return;
+        }
+
+        if (!contentElem) {
+          contentElem = elem[0].querySelector('.excerpt');
+
+          // Listen for events which might cause the size of the excerpt's
+          // content to change, even if the content data has not changed.
+
+          // This currently includes top-level window resize events and media
+          // (images, iframes) within the content loading.
+          contentElem.addEventListener('load', scheduleOverflowCheck,
+            true /* capture. 'load' events do not bubble */);
+
+          window.addEventListener('resize', scheduleOverflowCheck);
+        }
+
+        pendingOverflowCheck = false;
+        var overflowing = false;
+        if (ctrl.enabled) {
+          var hysteresisPx = ctrl.overflowHysteresis || 0;
+          overflowing = contentElem.scrollHeight >
+                 (ctrl.collapsedHeight + hysteresisPx);
+        }
+        if (overflowing === ctrl.overflowing) {
+          return;
+        }
+
+        ctrl.overflowing = overflowing;
+        if (ctrl.onCollapsibleChanged) {
+         ctrl.onCollapsibleChanged({collapsible: ctrl.overflowing});
+        }
+        //scope.$digest();
+      }
+
+      scope.$on('$destroy', function () {
+        pendingOverflowCheck = false;
+        window.removeEventListener('resize', scheduleOverflowCheck);
+      });
+
+      // Schedule a deferred check of whether the content is collapsed.
+      function scheduleOverflowCheck() {
+        if (pendingOverflowCheck) {
+          return;
+        }
+        pendingOverflowCheck = true;
+        requestAnimationFrame(function () {
+          recomputeOverflowState();
+          scope.$digest();
+        });
+      }
 
       ctrl.contentStyle = function contentStyle() {
-        if (!contentElem) {
+        if (!ctrl.enabled) {
           return {};
         }
 
         var maxHeight = '';
-        if (ctrl.overflowing()) {
+        if (ctrl.overflowing) {
           if (ctrl.collapse) {
             maxHeight = toPx(ctrl.collapsedHeight);
           } else if (ctrl.animate) {
-            // animating the height change requires that the final
+            // Animating the height change requires that the final
             // height be specified exactly, rather than relying on
             // auto height
+            var contentElem = elem[0].querySelector('.excerpt');
             maxHeight = toPx(contentElem.scrollHeight);
           }
+        } else if (typeof ctrl.overflowing === 'undefined' &&
+                   ctrl.collapse) {
+          // If the excerpt is collapsed but the overflowing state has not yet
+          // been computed then the exact max height is unknown, but it will be
+          // in the range [ctrl.collapsedHeight, ctrl.collapsedHeight +
+          // ctrl.overflowHysteresis]
+          //
+          // Here we guess that the final content height is most likely to be
+          // either less than `collapsedHeight` or more than `collapsedHeight` +
+          // `overflowHysteresis`, in which case it will be truncated to
+          // `collapsedHeight`.
+          maxHeight = toPx(ctrl.collapsedHeight);
         }
 
         return {
@@ -92,40 +168,27 @@ function excerpt() {
         };
       };
 
-      ctrl.overflowing = function overflowing() {
-        if (!contentElem) {
-          return false;
-        }
+      // Watch properties which may affect whether the excerpt
+      // needs to be collapsed and recompute the overflow state
+      scope.$watch('vm.contentData', scheduleOverflowCheck);
+      scope.$watch('vm.enabled()', scheduleOverflowCheck);
 
-        var hysteresisPx = ctrl.overflowHysteresis | 0;
-        return contentElem.scrollHeight >
-               (ctrl.collapsedHeight + hysteresisPx);
-      };
-
-      scope.$watch('vm.enabled()', function (isEnabled) {
-        if (isEnabled) {
-          contentElem = elem[0].querySelector('.excerpt');
-
-          // trigger an update of the excerpt when events happen
-          // outside of Angular's knowledge that might affect the content
-          // size. For now, the only event we handle is loading of
-          // embedded media or frames
-          contentElem.addEventListener('load', scope.$digest.bind(scope),
-            true /* capture. 'load' events do not bubble */);
-        } else {
-          contentElem = undefined;
-        }
-      });
-
-      scope.$watch('vm.overflowing()', function () {
-        if (ctrl.onCollapsibleChanged) {
-          ctrl.onCollapsibleChanged({collapsible: ctrl.overflowing()});
-        }
-      });
+      // Trigger an initial calculation of the overflow state.
+      //
+      // This is performed asynchronously so that the content of the <excerpt>
+      // has settled - ie. all Angular directives have been fully applied and
+      // the DOM has stopped changing. This may take several $digest cycles.
+      scheduleOverflowCheck();
     },
     scope: {
       /** Whether or not expansion should be animated. Defaults to true. */
       animate: '<?',
+      /**
+       * The data which is used to generate the excerpt's content.
+       * When this changes, the excerpt will recompute whether the content
+       * is overflowing.
+       */
+      contentData: '<',
       /** Whether or not truncation should be enabled */
       enabled: '&?',
       /**
